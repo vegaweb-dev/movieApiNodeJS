4dbae5ebe57cfb596c3cfae02d72ab1fcc06495a
carlos <carlosjuniorrvega@hotmail.com>
2022-06-20 15:39:51 -0500

    feat:sets and start the server

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+//Setttings
+app.set('port', 3000);
+
+//Middlewares
+app.use(morgan('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//Routes
+
+app.get('/', (req, res) => {
+	res.send('test');
+});
+
+//Starting the server
+app.listen(app.get('port'), () => {
+	console.log(`Server on port ${app.get('port')}`);
+});
